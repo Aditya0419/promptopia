8b583bb6bb87496affe2d2eeecf5192252cd0800
Aditya Mahajan <adityamahajan319@gmail.com>
2025-01-02 15:31:32 +0530

    other changes

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,6 @@
 import '@/app/globals.css'
+import Nav from '@/components/Nav.jsx'
+import Provider from '@/components/Provider.jsx'
 
 export const metadata = {
     title: 'Promptopia',
@@ -8,18 +10,21 @@ export const metadata = {
 const RootLayout = ({ children }) => {
     return (
         <html lang='en'>
+
             <body>
-                <div className='main'>
-                    <div className=" w-full dark:bg-black bg-white  dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2]">
-                        {/* <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" /> */}
+                <Provider>
+                    <div className='main'>
+                        <div className=" w-full dark:bg-black bg-white  dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2]">
+                            {/* <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" /> */}
 
+                        </div>
                     </div>
-                </div>
 
-                <main className="text-white z-5 relative">
-                    {children}
-                </main>
-
+                    <main className="text-white z-5 relative px-14">
+                        <Nav />
+                        {children}
+                    </main>
+                </Provider>
             </body>
         </html>
     )
