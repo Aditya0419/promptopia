c5f12a9b9d822f3ae155393424eda3295f64ab41
Aditya Mahajan <adityamahajan319@gmail.com>
2025-01-03 15:46:09 +0530

    wrapped useSearchParams in suspense to fix deploy error

--- src/app/update-prompt/page.jsx
+++ src/app/update-prompt/page.jsx
@@ -3,42 +3,43 @@
 import { useEffect, useState } from "react"
 import { useRouter } from "next/navigation"
 import { useSearchParams } from "next/navigation"
+import { Suspense } from 'react'
 
 import Form from "@/components/Form"
 
 const EditPrompt = () => {
-    const router=useRouter();
-    const searchParams=useSearchParams();
-    const promptId=searchParams.get('id');
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const promptId = searchParams.get('id');
     const [submitting, setIsSubmitting] = useState(false);
     const [post, setPost] = useState({ prompt: "", tag: "" });
 
-    useEffect(()=>{
-        const getpromptDetails=async ()=>{
-            const res=await fetch(`/api/prompt/${promptId}`)
-            const data=await res.json();
+    useEffect(() => {
+        const getpromptDetails = async () => {
+            const res = await fetch(`/api/prompt/${promptId}`)
+            const data = await res.json();
 
             setPost({
-                prompt:data.prompt,
-                tag:data.tag
+                prompt: data.prompt,
+                tag: data.tag
             })
         }
-        
-        if(promptId) getpromptDetails();
 
-    },[promptId])
+        if (promptId) getpromptDetails();
+
+    }, [promptId])
 
     const updatePrompt = async (e) => {
         e.preventDefault();
         setIsSubmitting(true);
 
-        if(!promptId) return alert('PromptID not found');
+        if (!promptId) return alert('PromptID not found');
 
         try {
             const res = await fetch(`/api/prompt/${promptId}`, {
                 method: 'PATCH',
                 body: JSON.stringify({
-                    prompt: post.prompt, 
+                    prompt: post.prompt,
                     tag: post.tag
                 })
             })
@@ -54,13 +55,16 @@ const EditPrompt = () => {
     }
 
     return (
-        <Form
-            type='Edit'
-            post={post}
-            setPost={setPost}
-            submitting={submitting}
-            handleSubmit={updatePrompt}
-        />
+        <Suspense>
+            <Form
+                type='Edit'
+                post={post}
+                setPost={setPost}
+                submitting={submitting}
+                handleSubmit={updatePrompt}
+            />
+        </Suspense>
+
     );
 }
 
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -12,7 +12,7 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
                 platform
             </p>
 
-            <form action=""
+            <form
                 onSubmit={handleSubmit}
                 className='mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism'
             >
